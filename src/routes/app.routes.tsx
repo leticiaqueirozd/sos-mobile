e0ebfe3626594cfb9893c695392c5442f0d64e2f
leticiaqueirozd <leticiaqsouza@outlook.com>
2024-07-05 00:53:58 -0300

    update

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -7,7 +7,6 @@ import {InitialScreen} from '../screens/InitialScreen';
 import {LoginScreen} from '../screens/LoginScreen';
 import {RegisterScreen} from '../screens/RegisterScreen';
 import {HomeScreen} from '../screens/HomeScreen';
-import {EmergencyContactsScreen} from '../screens/EmergencyContactsScreen';
 
 
 export type RootStackParamList = {
@@ -25,7 +24,6 @@ export function AppRoutes() {
         <Screen name="Login" component={LoginScreen}  />
         <Screen name = "Register" component={RegisterScreen}></Screen>
         <Screen name = "Home" component={HomeScreen} ></Screen>
-        <Screen name = "Emergência" component={EmergencyContactsScreen} ></Screen>
 
     </Navigator>
   )
